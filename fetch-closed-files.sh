bf658cd7f640bd1a4279ab9f774ee036a464831f
Dmitrii Izgurskii <izgurskii@gmail.com>
2014-05-28 16:42:51 +0200

    fetch-closed-files.sh accepts now 3 parameters

--- fetch-closed-files.sh
+++ fetch-closed-files.sh
@@ -4,9 +4,13 @@
 # This script fetches files from remote server that are closed.
 # We do not want open files because they may still be written to.
 
-host=$1
+host="$1"
+dir="$2"
+pattern="$3"
+
+echo "$dir/$pattern"
 files_to_fetch=$(ssh $host '
-        all_data_files=$(ls -1 ~/bitstamp-recorder/*bitstamp_data*)
+        all_data_files=$(ls -1 '$dir/$pattern')
         for file in $all_data_files; do
                 num_file_handles=$(/usr/sbin/lsof -f -- $file | wc -l)
                 if [ $num_file_handles -lt 1 ]; then echo $file; fi

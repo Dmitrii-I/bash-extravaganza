2a88796131771571941150335f4ec1c4b2f5cb77
Dmitrii Izgurskii <izgurskii@gmail.com>
2014-05-28 02:05:46 +0200

    added fetch-closed-files.sh

---
+++ fetch-closed-files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# This script fetches files from remote server that are closed.
+# We do not want open files because they may still be written to.
+
+host=$1
+files_to_fetch=$(ssh dima@$host '
+        all_data_files=$(ls -1 ~/bitstamp-recorder/*bitstamp_data*)
+        for file in $all_data_files; do
+                num_file_handles=$(/usr/sbin/lsof -f -- $file | wc -l)
+                if [ $num_file_handles -lt 1 ]; then echo $file; fi
+        done
+')
+
+echo Going to fetch these files:
+echo "$files_to_fetch"
+
+for file in $files_to_fetch; do
+        scp $host:$file ./
+done
+
+
+
+

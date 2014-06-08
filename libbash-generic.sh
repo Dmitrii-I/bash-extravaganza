6f7514fca66ecfda1e517fc9f79618eabebb991a
Dmitrii Izgurskii <izgurskii@gmail.com>
2014-06-08 12:23:36 +0000

    new function: is_login_shell

--- libbash-generic.sh
+++ libbash-generic.sh
@@ -3,4 +3,11 @@
 # Bash library containing generic functions
 
 
-timestamp=$(date +"%Y-%m-%d %H:%M:%S.%N")
+timestamp() {
+	date +"%Y-%m-%d %H:%M:%S.%N"
+}
+
+is_login_shell() {
+	if shopt -q login_shell; then echo TRUE
+	else echo FALSE; fi
+}

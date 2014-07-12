e8eb5d94b9e369abbd98497f1964eb44300ae541
Dmitrii Izgurskii <izgurskii@gmail.com>
2014-07-12 19:14:21 +0200

    mods

--- lib-bash-generic.sh
+++ lib-bash-generic.sh
@@ -19,9 +19,28 @@ closed_files() {
         # pattern should be between double quotes to prevent too early processing of meta-characters like *
         dir=${1:-.}
         pattern=${2:-*}
-        all_files=$(find $dir -maxdepth 1 -type f -name "$pattern")                                                 
+        all_files=$(find $dir -maxdepth 1 -type f -print -name "$pattern" | xargs -L1 readlink -f)                                                 
         for file in $all_files; do
                 num_file_handles=$(lsof -f -- $file | wc -l)
                 if [ $num_file_handles -lt 1 ]; then echo $file; fi
         done
+}
+
+download_files() {
+        while read -t 3 line; do
+                echo "$line"
+                scp -C "$line" .
+        done
+}
+
+
+prepend() {
+        prefix="$1"
+        while read -t 3 line; do
+                echo "${prefix}${line}"
+        done
+}
+
+s() {
+        source ~/bash-scripts/lib-bash-generic.sh
 }

c094f9c29de887042abb923b0ab16764c366b1d9
Dmitrii Izgurskii <izgurskii@gmail.com>
2014-07-12 16:10:09 +0200

    added function closed_files()

--- lib-bash-generic.sh
+++ lib-bash-generic.sh
@@ -11,3 +11,14 @@ is_login_shell() {
 	if shopt -q login_shell; then echo TRUE
 	else echo FALSE; fi
 }
+
+closed_files() {
+        # pattern should be between double quotes to prevent too early processing of meta-characters like *
+        dir=${1:-.}
+        pattern=${2:-*}
+        all_files=$(ls -1 $dir/$pattern)                                                 
+        for file in $all_files; do
+                num_file_handles=$(lsof -f -- $file | wc -l)
+                if [ $num_file_handles -lt 1 ]; then echo $file; fi
+        done
+}

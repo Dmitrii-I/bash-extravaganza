82c569119ea1f9ed86905b2ef109ec836cc8319a
Dmitrii Izgurskii <izgurskii@gmail.com>
2014-07-14 13:20:37 +0000

    closed_files now returns filename (basename) not the full file path

--- lib-bash-generic.sh
+++ lib-bash-generic.sh
@@ -19,7 +19,7 @@ closed_files() {
         # pattern should be between double quotes to prevent too early processing of meta-characters like *
         dir=${1:-.}
         pattern=${2:-*}
-        all_files=$(find $dir -maxdepth 1 -type f -name "$pattern" | xargs -L1 readlink -f)                                                 
+        all_files=$(find $dir -maxdepth 1 -type f -name "$pattern" | xargs -L1 basename)
         for file in $all_files; do
                 num_file_handles=$(lsof -f -- $file | wc -l)
                 [ $num_file_handles -lt 1 ] && echo $file

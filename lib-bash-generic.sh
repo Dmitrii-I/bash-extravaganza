29e0d3685c5fd8fccd36c41ff4bd7b2aff3e8885
Dmitrii Izgurskii <izgurskii@gmail.com>
2014-07-17 21:05:56 +0000

    fixed issue with non-zero exit code in closed_files

--- lib-bash-generic.sh
+++ lib-bash-generic.sh
@@ -26,10 +26,10 @@ closed_files() {
 
         # If there are no matched files, exit successfully, otherwise proceed
         [ -z "$all_files" ] && return 0 || all_files=$(echo "$all_files" | xargs -L1 basename)
-
+	echo "$all_files"
         for file in $all_files; do
                 num_file_handles=$(lsof -f -- $dir/$file | wc -l)
-                [ $num_file_handles -lt 1 ] && echo $file
+		if [ $num_file_handles -lt 1 ]; then echo $file; fi
         done
 }
 

65588c8c188fe06012bbc63dad87aabd0c285b89
Dmitrii Izgurskii <izgurskii@gmail.com>
2015-01-17 17:59:23 +0100

    adjusted the name of the script

--- run-forever.sh
+++ run-forever.sh
@@ -21,7 +21,7 @@
 #
 # To stop running forever, simply kill this script and the program
 
-LOG="./keep-running.log"
+LOG="./run-forever.log"
 CMD_TO_RUN="$@"
 COUNTER=1 # how many times the we have started the program
 
@@ -29,13 +29,13 @@ COUNTER=1 # how many times the we have started the program
 while true; do
         timestamp=$(date +"%Y-%m-%d %H:%M:%S.%N")
         echo -ne "$timestamp\t" >> $LOG
-        echo -ne "keep-running.sh\tAttempt #$COUNTER. Going to continuosly run: $CMD_TO_RUN\n" >> $LOG
+        echo -ne "run-forever.sh\tAttempt #$COUNTER. Going to continuosly run: $CMD_TO_RUN\n" >> $LOG
         $CMD_TO_RUN &> /dev/null
         sleep 3
         echo -ne "$timestamp\t" >> $LOG
-        echo -ne "keep-running.sh\tInstance #$COUNTER of $CMD_TO_RUN quit. Starting new instance\n" >> $LOG
+        echo -ne "run-forever.sh\tInstance #$COUNTER of $CMD_TO_RUN quit. Starting new instance\n" >> $LOG
         # bonus: send an email to ourselves 
-        echo "keep-running.sh: $CMD_TO_RUN has stopped. It has been started again." | ssmtp root &
+        echo "run-forever.sh: $CMD_TO_RUN has stopped. It has been started again." | ssmtp root &
         let "COUNTER=COUNTER+1"        
 done &> /dev/null
 ) &
